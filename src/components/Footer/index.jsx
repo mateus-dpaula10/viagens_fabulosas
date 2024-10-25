b0af1f04a5de1eee8517c48ea6f4bb21b9977637
Mateus De Paula <mateus.dpaula10@gmail.com>
2024-10-25 12:21:35 -0300

    Implementa Banner, Instagram e Footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -9,9 +9,7 @@ const Footer = () => {
     return (
         <footer className={styles.footer}>
             <div className="container">
-                <p>
-                    Copyright &copy; {year} - <a href="https://instagram.com/mateuss.dpaula10">Desenvolvido por Mateus De Paula</a>
-                </p>
+                <p>Copyright &copy; {year} - <a href="https://instagram.com/mateuss.dpaula10">Desenvolvido por Mateus De Paula</a></p>
             </div>
         </footer>
     )
